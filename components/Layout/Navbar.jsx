addfaa86f52c91caac270f902983442334b86c63
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2022-01-14 16:13:51 +0200

    Update Navbar.jsx

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -6,7 +6,7 @@ import { MenuIcon } from '@heroicons/react/solid';
 
 const Navbar = () => {
   return (
-    <nav className="max-w-5xl mx-auto flex items-center h-16 space-x-6">
+    <nav className="max-w-5xl mx-auto flex items-center justify-between h-16 space-x-6">
       <Link href="/" passHref>
         <img
           src="/images/logo.png"
@@ -14,29 +14,7 @@ const Navbar = () => {
           className="h-12 object-cover cursor-pointer"
         />
       </Link>
-      <div className="relative flex-1">
-        <div className="absolute right-0 text-gray-600 hover:text-gray-700 flex items-center pr-3 h-full cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </div>
-        <input
-          id="id"
-          className="text-gray-600 rounded-full bg-white dark:border-gray-700 font-normal h-10 flex items-center pl-3 text-sm border-gray-300 w-full border shadow"
-          placeholder="Search"
-        />
-      </div>
+      
       <Menu as="div" className="relative inline-block text-left z-50">
         <div className="flex items-center">
           <Menu.Button>
